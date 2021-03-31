0617d9b1d1df8f6532ba883d4e4183a746ebad5f
Corentin BRAVO <corentinbravo@gmail.com>
2021-03-31 10:45:56 +0200

    changements mineurs dans la façon dont sont récupérées les données dans la soupe

--- P2_scraping_une_page.py
+++ P2_scraping_une_page.py
@@ -7,52 +7,52 @@ response = requests.get(url)
 
 if response.ok :
     soup = BeautifulSoup(response.text, features="html.parser")
+    
     tags_to_search = {"UPC" : str(),
                       "Price (excl. tax)" : str(),
                       "Price (incl. tax)" :str() ,
                       "Availability" : str()}
+    
     possible_ratings = {"One" : "1",
                         "Two" : "2" ,
                         "Three" : "3",
                         "Four" : "4",
                         "Five" : "5"}
-    
-
-    rating_found = False
                         
 
-    content = soup.findAll('tr')
+    content = soup.findAll('tr') ##Trouve les quatres tags de tags_to_search.
     for tr in content :
         th = tr.find("th")
         if th.text in tags_to_search :
             td = tr.find("td")
             tags_to_search[th.text] = td.text
 
-    number_available = str()
+    number_available = str() ##Obtient et rend propre le nombre de livres disponibles.
     for char in tags_to_search["Availability"] :
-        if char.isnumeric :
+        if char.isnumeric() :
             number_available += char
 
-    content = soup.findAll('li')
+    content = soup.findAll('li')  ##Obtient la catégorie et le titre.
     category = content[2].find("a").text
     title = content[3].text
 
-    
+    description = soup.find("meta", {"name" : "description"}).get("content") ##Obtient la description. Il y a un peu de travail car il y a des espaces en trop au début
+    for k in range(len(description)) :
+        if description[k] != " " and description[k] != "\n" :
+            starting_index = k
+            break;
+    description = description[starting_index:]
+    description = "\'" + description  + "\'" #L'ajout des apostrophes permet à Libre Office de l'ouvrir correctement (avec les bons paramètres). Il y a peut-être moyen de faire mieux.
 
-    content = soup.findAll("p")
-    for p in content :
-        if p.text.startswith("\"") :
-            description = "\'" + p.text + "\'" ##L'ajout des apostrophes permet à Libre Office de l'ouvrir correctement (avec les bons paramètres). Il y a peut-être moyen de faire mieux.
-        if not rating_found :
-            for cl in p.get("class",list()) :
-                if cl in possible_ratings :
-                    rating = possible_ratings[cl]
-                    rating_found = True
+    classes = soup.find("p", {"class" : "star-rating"}).get("class") ##Obtient la note. Normalement la note du livre est toujours la première (celle des autres livres est à la fin)
+    for thing in classes :
+        if thing in possible_ratings :
+            rating = possible_ratings[thing]
 
-    if tags_to_search["Price (incl. tax)"][1] != "£" :
+    if tags_to_search["Price (incl. tax)"][0] != "£" :
         tags_to_search["Price (incl. tax)"] = tags_to_search["Price (incl. tax)"][1:]
-    if tags_to_search["Price (excl. tax)"][1] != "£" :
-        tags_to_search["Price (excl. tax)"] = tags_to_search["Price (excl. tax)"][1:] ##Résoud le problème d'encodage apparent s'il est présent. On verra pour trouver mieux.
+    if tags_to_search["Price (excl. tax)"][0] != "£" :
+        tags_to_search["Price (excl. tax)"] = tags_to_search["Price (excl. tax)"][1:] ##Résout le problème d'encodage apparent s'il est présent. On verra pour trouver mieux.
                     
     image = soup.find("img")["src"].replace("../..", "books.toscrape.com")
     
